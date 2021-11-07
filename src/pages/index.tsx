2306c895a5f8664d6541395d1fc93e0c23258484
CaioIbraim <caiofabiocosta@gmail.com>
2021-11-07 02:37:02 -0300

    correção

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,22 +69,35 @@ export default function Home({todos : severTodos}) {
         </h1>
 
 
-        <h2>Criar Tarefas</h2>
-        <input type="text" onChange={handleInputChange}  className="px-3 font-medium transition-colors duration-75 cursor-pointer border-2" value={inputTodo} />
-        <button onClick={handleCreateTodo}  className="px-3 font-medium transition-colors duration-75 cursor-pointer border-2 bg-indigo-500">Criar</button>  
-          <ul className="mt-8">
-            {todos.map((todo) => 
-              <li   className="px-3 font-medium transition-colors duration-75 cursor-pointer border-2"
-              key={todo.id || todo.task}
-              onClick={() => handleUpdateTodo(todo.id)}
-              >
-                <p className={todo.status && 'line-through'}>
-                 {todo.task}
-                 </p>
-                 <button onClick={() => handleDeleteTOdo(todo.id)}  className="px-3 font-medium transition-colors duration-75 cursor-pointer border-2 bg-red-500">Remover</button>  
-              </li>
-            )}
-          </ul>
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+			<h1 className="mb-16 text-4xl font-bold text-gray-800">To-do</h1>
+			<h2 className="mb-8 text-2xl font-medium text-gray-800">Criar to-do</h2>
+			<div className="flex">
+				<input
+					className="px-2 font-medium transition-colors duration-100 border-2 border-blue-400 rounded-lg outline-none focus:border-green-500"
+					type="text"
+					onChange={handleInputChange}
+					value={inputTodo}
+				/>
+				<button
+					className="px-4 mx-2 text-lg font-medium text-white transition-colors duration-200 bg-blue-500 rounded-lg duration-20transition-colors hover:bg-blue-700"
+					onClick={handleCreateTodo}>
+					Criar
+				</button>
+			</div>
+			<ul className="mt-8">
+				{todos.map((todo) => (
+					<li
+						className="px-3 font-medium transition-colors duration-150 bg-white border-2 border-blue-400 rounded-lg cursor-pointer hover:bg-blue-100"
+						key={todo.id || todo.task}
+						onClick={() => handleUpdateTodo(todo.id)}>
+						<p className={`${todo.status && "line-through"}`}>{todo.task}</p>
+					</li>
+				))}
+			</ul>
+		</div>
+
+        
       </main>
     </div>
   )
--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({baseURL: "http://localhost:3000/api"})
+let base = process.env["BASE_URL_API"];
+
+const api = axios.create({baseURL: base })
 export default api
